2b01f7bcf57fc4482e96e6ff90219f6033def587
Tommy Li <Tommy.chaoping.li@ibm.com>
2017-09-27 12:14:18 -0700

    add back script for deploy to bluemix button

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+cf create-service iotf-service iotf-service-free iotp-for-conveyor
+git clone https://github.com/ibm-watson-iot/guide-conveyor-simulator
+cd guide-conveyor-simulator
+cf push conveyorbelt-iotp --no-start
+cf bind-service conveyorbelt-iotp iotp-for-conveyor
+cf restage conveyorbelt-iotp
+cf start conveyorbelt-iotp
