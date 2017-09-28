7d8600db2edb0b3dd845da7fdc9efc8ad05e0fe9
Tommy Li <Tommy.chaoping.li@ibm.com>
2017-09-28 13:39:28 -0700

    fix link, remove script

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-cf create-service iotf-service iotf-service-free iotp-for-conveyor
-cf push conveyorbelt-iotp --no-start
-cf bind-service conveyorbelt-iotp iotp-for-conveyor
-cf restage conveyorbelt-iotp
-cf start conveyorbelt-iotp
